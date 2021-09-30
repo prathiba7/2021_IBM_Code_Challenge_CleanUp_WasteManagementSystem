19adfc4b39d76b7eb1286dd5f158e340b5d5d2fc
sarathp20 <53684842+sarathp20@users.noreply.github.com>
2021-10-01 00:59:14 +0530

    ui3

--- app/src/main/java/com/example/cleanup/Admin.java
+++ app/src/main/java/com/example/cleanup/Admin.java
@@ -1,14 +1,20 @@
 package com.example.cleanup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Admin extends AppCompatActivity {
     private Button loc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +30,24 @@ public class Admin extends AppCompatActivity {
                                      }
                                  }
         );
+
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    // this event will enable the back
+    // function to the button on press
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
 
     }
 }
--- app/src/main/java/com/example/cleanup/MainActivity.java
+++ app/src/main/java/com/example/cleanup/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         bworker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,WorkerActivity.class);
+                Intent intent=new Intent(MainActivity.this,WorkerInfo.class);
                 startActivity(intent);
                 finish();
                 return;
